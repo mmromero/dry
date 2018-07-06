cda1512799f6b23acfca42818a37391e8ef0abd6
Miguel <miguel.molina@tum.de>
2018-07-06 11:39:20 +0200

    Functional Keras Sequential model to predict tissue volume fraction and correct for free-water contamination.

--- dry/example.py
+++ dry/example.py
@@ -1,8 +1,17 @@
-from dry import Dry
+from dry.dry import Dry
 
 if __name__ == "__main__":
-    bfile = 'tests/test_ok.bvals'
-    dwi = 'data/test.nii.gz'
+    bfile = 'dry/data/one-shell/test_ok.bvals'
+    dwi = ['dry/data/one-shell/test1.nii.gz',
+           'dry/data/one-shell/test2.nii.gz',
+           'dry/data/one-shell/test3.nii.gz']
     dann = Dry()
-    model = dann.train_model(bfile)
-    dann.correct_fwe(dwi, model, output_folder='tests')
+    model1s = dann.train_model(bfile)
+    dann.correct_fwe(dwi, model1s, bfile, output_folder='dry/data')
+
+    bfile = 'dry/data/two-shell/merged.bval'
+    dwi = ['dry/data/two-shell/merged1.nii.gz',
+           'dry/data/two-shell/merged2.nii.gz']
+    dann = Dry()
+    model2s = dann.train_model(bfile)
+    dann.correct_fwe(dwi, model2s, bfile, output_folder='dry/data')
